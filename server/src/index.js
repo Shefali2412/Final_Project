1ec4a977c0ef89d876cd37fb2bd305667901fdb9
gofordivya <gofordivya@gmail.com>
2024-01-09 21:40:48 +0100

    fixed server side

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import  {userRouter } from '/Users/divya/Desktop/MERN-STACK/server/routes/users.js';
-
+import { userRouter } from "../routes/users.js";
+import { recipesRouter } from "../routes/recipes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(
   "mongodb+srv://gofordivya:ErpiAa2NrXN2aerp@recipes.0fkfcya.mongodb.net/recipes?retryWrites=true&w=majority",
