e1eba85a17f463c179416039640a30650537bff8
gofordivya <gofordivya@gmail.com>
2024-01-09 21:52:51 +0100

    fix for client side

--- client/my-react-app/src/pages/home.js
+++ client/my-react-app/src/pages/home.js
@@ -1,68 +1,75 @@
-import Header from "../components/header";
-import Footer from "../components/footer";
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
 
 export const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/ids/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchRecipes();
+    fetchSavedRecipes();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put("http://localhost:3001/recipes", {
+        recipeID,
+        userID,
+      });
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const isRecipeSaved = (id) => savedRecipes.includes(id);
+
   return (
-    <div className="home-container">
-      <Header />
-      <section className="introduction-section">
-        <h1>Welcome to RecipeHub</h1>
-        <p>
-          Your ultimate destination for culinary inspiration and organization.
-          Discover, create, and savor delightful recipes in one central hub.
-        </p>
-      </section>
-
-      <section className="why-recipehub-section">
-        <h2>Why RecipeHub?</h2>
-        <div className="feature">
-          <h3>Effortless Organization</h3>
-          <p>
-            Streamline your recipe collection with easy-to-use tools for adding,
-            editing, and categorizing your favorite dishes.
-          </p>
-        </div>
-        <div className="feature">
-          <h3>Inspiration Galore</h3>
-          <p>
-            Explore a vast array of recipes from diverse cuisines to spark your
-            creativity in the kitchen.
-          </p>
-        </div>
-        <div className="feature">
-          <h3>Seamless Cooking</h3>
-          <p>
-            Access your recipes anytime, anywhere, making meal planning and
-            cooking a breeze.
-          </p>
-        </div>
-      </section>
-
-      <section className="key-features-section">
-        <h2>Key Features</h2>
-        <div className="feature">
-          <h3>Recipe Management</h3>
-          <p>
-            Effortlessly add, edit, and delete recipes with our intuitive
-            interface.
-          </p>
-        </div>
-        <div className="feature">
-          <h3>Categorization</h3>
-          <p>
-            Organize your recipes by categories to quickly find the perfect dish
-            for any occasion.
-          </p>
-        </div>
-        <div className="feature">
-          <h3>User-Friendly</h3>
-          <p>
-            Enjoy a user-friendly experience designed to enhance your culinary
-            journey.
-          </p>
-        </div>
-      </section>
-      <Footer />
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <button
+                onClick={() => saveRecipe(recipe._id)}
+                disabled={isRecipeSaved(recipe._id)}
+              >
+                {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
+              </button>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
